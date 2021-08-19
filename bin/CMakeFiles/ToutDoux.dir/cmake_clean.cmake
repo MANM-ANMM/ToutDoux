c45c469ab1bb0a2c407bf4c1ea7c5cdc30a8cac8
marouault <martin.rouault@etu.uca.fr>
2021-08-19 14:08:33 +0200

    SetUp de Cmake

---
+++ bin/CMakeFiles/ToutDoux.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ToutDoux.dir/source/main.cpp.obj"
+  "CMakeFiles/ToutDoux.dir/source/main.cpp.obj.d"
+  "ToutDoux.exe"
+  "ToutDoux.exe.manifest"
+  "ToutDoux.pdb"
+  "libToutDoux.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ToutDoux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bin/CMakeFiles/ToutDoux.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ToutDoux.
+# This may be replaced when dependencies are built.
